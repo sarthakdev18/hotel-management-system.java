0dcad70110da37f82c1f7486b06c81e1f7728aa5
Sarthak Dev <91304126+sarthakdev18@users.noreply.github.com>
2023-09-04 17:33:41 +0530

    Add files via upload

---
+++ Hotel management system/hotel/management/system/DbUtils.java
@@ -0,0 +1,13 @@
+package hotel.management.system;
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+public enum DbUtils {
+    ;
+
+    public static TableModel resultSetToTableModel(ResultSet rs) {
+        return null;
+    }
+
+}
